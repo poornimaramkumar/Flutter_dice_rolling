90536088398ccba89e39bd103f2138c56815b6f7
Poornima Konda Ramkumar <poornima.ramkumar@accolade.com>
2024-10-06 22:36:35 -0400

    dice roll working

---
+++ android/app/src/main/kotlin/co/appbrewery/flutter_dice_rolling/MainActivity.kt
@@ -0,0 +1,5 @@
+package co.appbrewery.flutter_dice_rolling
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.blue,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.white,
         ),
         body: DicePage(),
       ),
@@ -15,9 +16,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    changeDiceFace();
+                  },
+                  child: Image.asset('images/dice$rightDiceNumber.png'))),
+        ],
+      ),
+    );
   }
 }
